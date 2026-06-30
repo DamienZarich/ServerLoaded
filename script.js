072b7c82172a59eb4240c152527ee2af6c7f16d3
Damien Zarich <damienzarich@gmail.com>
2026-06-30 12:55:01 -0700

    when you click the start/stop server and throws you back a message

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function updateStatus(isOnline) {
         indicator.className = 'status-offline';
     }
 }
+function sendCommand(cmd) {
+    const btn = event.target;
+    const originalText = btn.innerText;
+
+    btn.innerText = "EXECUTING...";
+    btn.style.opacity = "0.5";
+    btn.disabled = true;
+
+    setTimeout(() => {
+        btn.innerText = originalText;
+        btn.style.opacity = "1";
+        btn.disabled = false;
+        
+        logEvent(`System: Command '${cmd.toUpperCase()}' executed successfully.`);
+        
+        if(cmd === 'start') {
+            updateStatus(true);
+        } else if (cmd === 'stop') {
+            updateStatus(false);
+        }
+    }, 1500);
+}
