809b058fd67de83821173fb16f54fcf8184c9e8c
Damien Zarich <damienzarich@gmail.com>
2026-06-30 12:51:13 -0700

    created js for when your server is on/off

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateStatus(isOnline) {
+    const indicator = document.getElementById('status-Indicator');
+    if (isOnline) {
+        indicator.className = 'status-online';
+    } else {
+        indicator.className = 'status-offline';
+    }
+}
