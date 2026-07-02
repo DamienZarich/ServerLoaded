657a9026f8d7c2c1403875d98ea840e002a0313b
Damien Zarich <damienzarich@gmail.com>
2026-07-02 01:10:46 -0700

    Updated main.js logic so now in the future file path can be read to locate server files

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs')
 
+let mainWindow;
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
