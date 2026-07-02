ed614d9e151742aa2f2b1d7831cac3c68d7eb67c
Damien Zarich <damienzarich@gmail.com>
2026-07-02 11:15:10 -0700

    added new divs these will work like this: one bar the bakc one will be used to display TOTAL system usage. The front bar on the other hand will be used to display the system resources used by ONLY the server.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateStatus(isOnline) {
         indicator.className = 'status-offline';
     }
 }
-function sendCommand(cmd) {
+async function sendCommand(cmd) {
     const btn = event.target;
     const originalText = btn.innerText;
 
@@ -14,19 +14,14 @@ function sendCommand(cmd) {
     btn.style.opacity = "0.5";
     btn.disabled = true;
 
-    setTimeout(() => {
-        btn.innerText = originalText;
-        btn.style.opacity = "1";
-        btn.disabled = false;
-        
-        logEvent(`System: Command '${cmd.toUpperCase()}' executed successfully.`);
-        
-        if(cmd === 'start') {
-            updateStatus(true);
-        } else if (cmd === 'stop') {
-            updateStatus(false);
-        }
-    }, 1500);
+   const response = await window.electronAPI.sendCommand(cmd)
+   if (response.success) {
+    logEvent(`System: ${response.message}`);
+   }
+   setTimeout(() => {
+    btn.innerText = originalText;
+    btn.disabled = false;
+   }, 1000);
 }
 function logEvent(message) {
     const logWindow = document.getElementById('log-window');
@@ -46,5 +41,14 @@ setInterval(async () => {
     if (cpuBar && membar) {
         cpuBar.style.width = stats.cpu
         membar.style.width = stats.memory
+        const cpuVal = parseInt(stats.cpu)
+        const memVal = parseInt(stats.memory)
+        cpuBar.classList.remove('high-load', 'med-load')
+        membar.classList.remove('high-load', 'med-load')
+        if (cpuVal > 70) cpuBar.classList.add('high-load');
+        else if (cpuVal > 45) cpuBar.classList.add('med-load');
+
+        if (memVal > 70) membar.classList.add('high-load');
+        else if(memVal > 45) membar.classList.add('med-load')
     }
 }, 2000);
